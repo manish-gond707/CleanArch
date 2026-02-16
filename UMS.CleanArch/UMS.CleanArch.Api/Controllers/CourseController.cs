8d141763c99f92d0947021995686db540d43b530
Manish Gond <manish.gond@tmotions.com>
2026-02-16 15:00:48 +0530

    Add Api Project and Reanme Controller

---
+++ UMS.CleanArch/UMS.CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UMS.CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+        
+    }
+}
